a8e9a57c2e2c9c8d44ab5dbb625e3b3ea2f490d8
xudenden <56295612@qq.com>
2020-03-11 21:35:58 +0800

    what the

--- AIPlayer.py
+++ AIPlayer.py
@@ -21,6 +21,16 @@ class AIPlayer:
         self.weight[1:-1, 1:-1] = np.full((6, 6), 0.85)
         self.weight[2:-2, 2:-2] = np.full((4, 4), 0.75)
         self.weight[3:-3, 3:-3] = np.full((2, 2), 0.65)
+        self.weight[0:2, 0:2] += np.full((2, 2), 0.1)
+        self.weight[0:2, -2::] += np.full((2, 2), 0.1)
+        self.weight[-2::, -2::] += np.full((2, 2), 0.1)
+        self.weight[-2::, 0:2] += np.full((2, 2), 0.1)
+        self.history = np.zeros((2, 64, 8, 8))
+        for i in range(2):
+            for j in range(64):
+                for k in range(8):
+                    for l in range(8):
+                        self.history[i, j, k, l] = k * 10 + l
 
     def get_move(self, board):
         """
@@ -40,31 +50,50 @@ class AIPlayer:
         max_val = self.min_val  # Best reward for current situation under limited moves, initialized with a small enough value
         max_index = 0  # index of the best move
         moves = list(board.get_legal_actions(self.color))  # list of possible moves
+        step = 0
+        # step = board.count(self.color)+board.count(self.oppo_color(self.color))
+        # self.depth -= step
         for index, move in enumerate(moves):
             alpha = self.min_val  # Initialize alpha with a small enough value
             beta = self.max_val  # Initialize beta with a big enough value
             flipped = board._move(move, self.color)  # Do the move and remember the pieces got flipped for backpropagation
-            val = -self.alpha_beta(board, alpha, beta, self.color, self.depth)  # calculate best reward after making this move
+            val = -self.alpha_beta(board, alpha, beta, self.color, self.depth, step)  # calculate best reward after making this move
             board.backpropagation(move, flipped, self.color)  # Do backpropagation to restore board state
             max_val = max(val, max_val)  # Update max value
             max_index = index  # Update index of max value
 
         return moves[max_index]
 
-    def pos_color(self, color):
+    def oppo_color(self, color):
         return "X" if color is "O" else "O"  # get the color of your opponent
 
     def sign_color(self, color):
         return 1 if color is self.color else -1  # get the sign of a particular color, positive if equal to self.color
+
     def value_color(self, color):
-        return 1 if color is self.color else (-1 if color is self.pos_color(color) else 0)
+        return 1 if color is self.color else (-1 if color is self.oppo_color(color) else 0)
 
     def evaluate(self, board, color):
         result = 0
-        for i in range(8):
-            for j in range(8):
-                result += self.value_color(board._board[i][j]) * self.weight[i][j]
+        result = board.count(color) - board.count(self.oppo_color(color))
+        # for i in range(8):
+        #     for j in range(8):
+        #         result += self.value_color(board._board[i][j]) * self.weight[i][j]
         return self.sign_color(color) * result
+
+    def mark_best(self, color_index, step, move):
+        pass
+
+    def mark_avg(self, color_index, step, move):
+        pass
+
+    def sort_by_history(self, board, color_index, step, moves):
+        weights = np.zeros((len(moves),))
+        for i in range(len(moves)):
+            move = board.board_num(moves[i])
+            weights[i] = self.history[color_index, step, move[0], move[-1]]
+        ids = np.argsort(weights)
+        return np.asarray(moves)[ids]
 
     def alpha_beta(self, board, alpha, beta, color, depth, step):
         """
@@ -90,7 +119,8 @@ class AIPlayer:
         """
         max_val = self.min_val  # Initializing the return value to be a small enough number, defined in init
         moves = list(board.get_legal_actions(color))  # Get possible moves of current color for future reference
-        pos_moves = list(board.get_legal_actions(self.pos_color(color)))  # Get possible moves of the opponent's color for future reference
+        pos_moves = list(board.get_legal_actions(self.oppo_color(color)))  # Get possible moves of the opponent's color for future reference
+        color_index = (self.sign_color(color) + 1) // 2
 
         # TERMINATION POINT
         if depth <= 0:
@@ -99,15 +129,19 @@ class AIPlayer:
             if len(pos_moves) is 0:
                 return self.evaluate(board, color)  # Game is finished
             # POSSIBLE RECURSION
-            return -self.alpha_beta(board, -beta, -alpha, self.pos_color(color), depth, step)  # Cannot move, wait for opponent's reaction
+            return -self.alpha_beta(board, -beta, -alpha, self.oppo_color(color), depth, step)  # Cannot move, wait for opponent's reaction
+        # moves = self.sort_by_history(board, color_index, step, moves)
         for move in moves:
             flipped = board._move(move, color)  # Save flipped pieces for backpropation
             # POSSIBLE RECURSION
-            val = -self.alpha_beta(board, -beta, -alpha, self.pos_color(color), depth - 1, step)  # Move one depth forward
+            val = -self.alpha_beta(board, -beta, -alpha, self.oppo_color(color), depth - 1, step)  # Move one depth forward
             board.backpropagation(move, flipped, color)  # Restore board state
             max_val = max(max_val, val)
-            if max_val >= beta:
-                # print("Pruning")
-                return max_val
-            alpha = max(max_val, alpha)
+            if max_val > alpha:
+                if max_val >= beta:
+                    # print("Pruning")
+                    return max_val
+                    # self.mark_best(color_index, step+self.depth-depth, move)
+                alpha = max_val
+                # self.mark_avg(color_index, step+self.depth-depth, move)
         return max_val
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # 导入黑白棋文件
 from game import Game  
 from RandomPlayer import RandomPlayer
+from HumanPlayer import HumanPlayer
 from AIPlayer import AIPlayer
 # 人类玩家黑棋初始化
-black_player = RandomPlayer("X")
+black_player = HumanPlayer("X")
+black_player = RandomPlayer("O")
 
 # AI 玩家 白棋初始化
 white_player = AIPlayer("O", max_depth=3)
+# white_player = RandomPlayer("O")
 
 # 游戏初始化，第一个玩家是黑棋，第二个玩家是白棋
 game = Game(black_player, white_player)
