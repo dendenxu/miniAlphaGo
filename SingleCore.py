8026b23df5d52ce249e15558d532bc7a43d2cd18
xudenden <56295612@qq.com>
2020-03-12 20:37:22 +0800

    history done

--- SingleCore.py
+++ SingleCore.py
@@ -1,14 +1,16 @@
 import numpy as np
+from random import shuffle
 
 
 class AIPlayer:
-    def __init__(self, color, big_val=1e10, small_val=-1e10, max_depth=3):
+    def __init__(self, color, big_val=1e10, small_val=-1e10, max_depth=3, max_width=10):
         self.action = None
         self.color = color
         self.oppo_color = "X" if color is "O" else "O"
         self.big_val = big_val
         self.small_val = small_val
         self.depth = max_depth
+        self.max_width = max_width
         self.weight = (np.asarray([[90, -30, 10, 10, 10, 10, -30, 90],
                                    [-30, -80, 5, 5, 5, 5, -80, -30],
                                    [10, 5, 1, 1, 1, 1, 5, 10],
@@ -18,11 +20,13 @@ class AIPlayer:
                                    [-30, -80, 5, 5, 5, 5, -80, -30],
                                    [90, -30, 10, 10, 10, 10, -30, 90]]))
         self.factor = abs(np.average(np.average(self.weight)))
+        self.history = np.tile(np.arange(64), 128).reshape((2, 64, 64))
 
     def get_move(self, board):
         player_name = '黑棋' if self.color == 'X' else '白棋'
         print("请等一会，对方 {}-{} 正在思考中...".format(player_name, self.color))
-        return self.alpha_beta(board, self.small_val, self.big_val, self.color, self.depth)[1]
+        return self.alpha_beta(board, self.small_val, self.big_val,
+                               self.color, self.depth, board.count("X") + board.count("O"))[1]
 
     def evaluate(self, board, color, oppo_color):
         weight = self.weight
@@ -44,7 +48,7 @@ class AIPlayer:
         _board += stability * self.factor
         return _board
 
-    def alpha_beta(self, board, alpha, beta, color, depth):
+    def alpha_beta(self, board, alpha, beta, color, depth, step):
         action = None
         oppo_color = "X" if color is "O" else "O"
         max_val = self.small_val
@@ -57,10 +61,13 @@ class AIPlayer:
             if len(oppo_moves) is 0:
                 mobility = (len(moves) - len(oppo_moves)) * self.factor
                 return self.evaluate(board, color, oppo_color) + mobility, action
-            return -self.alpha_beta(board, -beta, -alpha, oppo_color, depth)[0], action
+            return -self.alpha_beta(board, -beta, -alpha, oppo_color, depth, step)[0], action
+        global_depth = step + self.depth - depth
+        moves = self.history_sort(board, moves, color, global_depth)
+        moves = moves[0:min(len(moves), self.max_width)]
         for move in moves:
             flipped = board._move(move, color)
-            val = -self.alpha_beta(board, -beta, -alpha, oppo_color, depth - 1)[0]
+            val = -self.alpha_beta(board, -beta, -alpha, oppo_color, depth - 1, step)[0]
             board.backpropagation(move, flipped, color)
             if val > max_val:
                 max_val = val
@@ -68,6 +75,26 @@ class AIPlayer:
             if max_val > alpha:
                 if max_val >= beta:
                     action = move
+                    self.reward_move(board, action, color, global_depth, True)
                     return max_val, action
                 alpha = max_val
+        self.reward_move(board, action, color, global_depth, False)
         return max_val, action
+
+    def history_sort(self, board, moves, color, depth):
+        poss = list(map(lambda x: x[0] * 8 + x[1], [board.board_num(move) for move in moves]))
+        values = self.history[int(color == self.color), depth][poss]
+        idx = np.argsort(values)
+        return np.asarray(moves)[idx]
+
+    def reward_move(self, board, move, color, depth, best):
+        x, y = board.board_num(move)
+        pos = x * 8 + y
+        color = int(color == self.color)
+        val = self.history[color, depth, pos]
+        other_pos = np.argwhere(self.history[color, depth, :] ==
+                                val - (val if best else (1 if val else 0)))
+        self.history[color, depth, other_pos], \
+        self.history[color, depth, pos] = \
+            self.history[color, depth, pos], \
+            self.history[color, depth, other_pos]
--- main.py
+++ main.py
@@ -7,23 +7,28 @@ if __name__ == '__main__':
     import SimpleAIPlayer
     import MultiCore
     import SingleCore
-    # 人类玩家黑棋初始化
-    # black_player = HumanPlayer("X")
-    # black_player = RandomPlayer("X")
-    black_player = SimpleAIPlayer.AIPlayer("X", max_depth=3)
-    # black_player = AIPlayer.AIPlayer("X", max_depth=3)
 
-    # AI 玩家 白棋初始化
-    white_player = AIPlayer.AIPlayer("O", max_depth=3)
-    # white_player = SimpleAIPlayer.AIPlayer("O")
-    # white_player = RandomPlayer("O")
-
+    max_depth_black = 2
+    max_depth_white = 5
     while True:
+        # 人类玩家黑棋初始化
+        # black_player = HumanPlayer("X")
+        # black_player = RandomPlayer("X")
+        black_player = SimpleAIPlayer.AIPlayer("X", max_depth=max_depth_black)
+        # black_player = AIPlayer.AIPlayer("X", max_depth=3)
+
+        # AI 玩家 白棋初始化
+        white_player = SingleCore.AIPlayer("O", max_depth=max_depth_white)
+        # white_player = SimpleAIPlayer.AIPlayer("O")
+        # white_player = RandomPlayer("O")
         # 游戏初始化，第一个玩家是黑棋，第二个玩家是白棋
         game = Game(black_player, white_player)
 
         # 开始下棋
         game.run()
-        again = input("兄贵，再试一次？（狗头）：yes/no: ")
+        again = input("兄贵，再试一次？（狗头）：yes/no/change max_depth: ")
         if again[0] == 'N' or again[0] == 'n':
             break
+        if again[0] == 'C' or again[0] == "c":
+            max_depth_black = int(input("Max depth for black: "))
+            max_depth_white = int(input("Max depth for white: "))
